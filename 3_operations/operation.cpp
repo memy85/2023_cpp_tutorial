64983f8642f47bc30ea607a37e3161900c5b62d4
Won Seok Jang <memy85@github.com>
2023-01-14 19:33:44 +0900

    operations

---
+++ 3_operations/operation.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(void){
+
+	// 산술연산자 +, -, *, / %
+	// 비교연산자 >, <, ==, !=, >=, <=
+	// 증강 ++ --
+	// 논리 && || !
+	// 조건 ? : 삼항 연산자
+	//
+	double my_variable = 10;
+	double coverable = 2;
+
+	my_variable = my_variable * coverable;
+	printf("this is my_variable %f\n", my_variable);
+}
